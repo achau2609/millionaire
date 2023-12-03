357bda60e59f8c84fa7ab71ed7768c783c9f5053
Yan344933 <79313082+Yan344933@users.noreply.github.com>
2023-12-02 19:37:23 -0500

    Questions
    - Add QuestionReader class
    - Modify Question getCorrectAnswer

---
+++ src/QuestionReader.java
@@ -0,0 +1,57 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class QuestionReader {
+	
+	ArrayList<Question> questions;
+	
+	QuestionReader() throws FileNotFoundException{
+		
+		try {
+			File file = new File("src/week5/scores.txt");
+			
+			Scanner input = new Scanner(file);
+			
+			String[] answers = new String[4];
+			while (input.hasNext()) {
+				
+				
+                String questionText = input.nextLine();
+                String hint = input.nextLine();
+                answers[0] = input.nextLine();
+                answers[1] = input.nextLine();
+                answers[2] = input.nextLine();
+                answers[3] = input.nextLine();
+                String answer = input.nextLine();
+                // Get correct answer ABCD
+                char correctAnswer = answer.substring(8, 1).charAt(0);
+                int correctIndex = (int) correctAnswer - (int)'A';
+                questions.add(new Question(questionText, answers, correctIndex, hint));
+            }
+			
+			input.close();
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			
+		}
+		
+	}
+	
+	public Question[] getQuestions(int NumberOfQuestions) {
+		Random rand = new Random();
+		
+		Question[] questions = new Question[NumberOfQuestions];
+		
+		for (int i = 0; i < NumberOfQuestions; i++) {
+			int index = rand.nextInt(this.questions.size());
+			
+			questions[i] = this.questions.get(index);
+			
+			this.questions.remove(index);
+		}
+		
+		return questions;
+	}
+}
